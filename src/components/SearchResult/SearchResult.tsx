cec677d7c90faad56e3c30480552f1507a614a2d
arshak-grigoryan <arshak.code@gmail.com>
2023-12-11 22:09:45 +0400

    fix render conditions

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -16,28 +16,55 @@ export default function SearchResult() {
 
   const showPagination = data && data.total > perPage;
 
-  if (isFetching) {
-    return <div>Loading...</div>;
+  if (isFetching || !isFetched) {
+    return <div>skeleton.</div>;
   }
-  if (isFetched) {
-    if (error) {
-      <div>{error.message}</div>;
-    } else {
-      if (data) {
-        return (
-          <Box>
-            <Box
-              sx={{ backgroundColor: '#f9f9f9' }}
-              borderTop="1px solid rgba(0,0,0,.1)"
-              borderBottom="1px solid rgba(0,0,0,.1)"
-              py={2}
-              px={4}
-              display={'flex'}
-              alignItems={'center'}
-              justifyContent={'space-between'}
-            >
-              <Typography>{data.total} packages found</Typography>
-              {showPagination && (
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (data) {
+    return (
+      <Box>
+        <Box
+          sx={{ backgroundColor: '#f9f9f9' }}
+          borderTop="1px solid rgba(0,0,0,.1)"
+          borderBottom="1px solid rgba(0,0,0,.1)"
+          py={2}
+          px={4}
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+        >
+          <Typography>{data.total} packages found</Typography>
+          {showPagination && (
+            <Pagination
+              count={Math.ceil(data.total / perPage)}
+              page={page}
+              onChange={(_, page) => {
+                setPage(page);
+                const from = page * perPage - perPage;
+                setSearchParams((params) => {
+                  params.set('from', String(from));
+                  return params;
+                });
+              }}
+            />
+          )}
+        </Box>
+        <Stack direction={'row'} gap={4} px={4} py={2}>
+          <Box width={250}>
+            <SortOptions />
+          </Box>
+          <Stack width={'calc(100% - 250px)'}>
+            <Box>
+              {data.objects.map((obj) => (
+                <ListPackage key={obj.package.name} obj={obj} />
+              ))}
+            </Box>
+            {showPagination && (
+              <Box py={2}>
                 <Pagination
                   count={Math.ceil(data.total / perPage)}
                   page={page}
@@ -50,41 +77,13 @@ export default function SearchResult() {
                     });
                   }}
                 />
-              )}
-            </Box>
-            <Stack direction={'row'} gap={4} px={4} py={2}>
-              <Box width={250}>
-                <SortOptions />
               </Box>
-              <Stack width={'calc(100% - 250px)'}>
-                <Box>
-                  {data.objects.map((obj) => (
-                    <ListPackage key={obj.package.name} obj={obj} />
-                  ))}
-                </Box>
-                {showPagination && (
-                  <Box py={2}>
-                    <Pagination
-                      count={Math.ceil(data.total / perPage)}
-                      page={page}
-                      onChange={(_, page) => {
-                        setPage(page);
-                        const from = page * perPage - perPage;
-                        setSearchParams((params) => {
-                          params.set('from', String(from));
-                          return params;
-                        });
-                      }}
-                    />
-                  </Box>
-                )}
-              </Stack>
-            </Stack>
-          </Box>
-        );
-      }
-    }
+            )}
+          </Stack>
+        </Stack>
+      </Box>
+    );
   }
 
-  return null;
+  return <div>No data</div>;
 }
