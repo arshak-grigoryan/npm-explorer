438f11eb7fe6d5fdf45bd9bb6958559e367e28e0
arshak-grigoryan <arshak.code@gmail.com>
2024-01-09 19:59:55 +0400

    chore: add type checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isDev = mode === 'development';
-  return { plugins: [svgr(), react()], base: isDev ? '/' : '/npm-explorer/' };
+  return {
+    plugins: [svgr(), react(), checker({ typescript: true })],
+    base: isDev ? '/' : '/npm-explorer/',
+  };
 });
